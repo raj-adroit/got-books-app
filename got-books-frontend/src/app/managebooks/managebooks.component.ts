cf5e4f2313d5e292ba918e7eabe493eb519d2988
Rajendran S <rajendran@machinevantage.com>
2019-01-09 14:43:53 +0530

    refactor code

--- got-books-frontend/src/app/managebooks/managebooks.component.ts
+++ got-books-frontend/src/app/managebooks/managebooks.component.ts
@@ -13,23 +13,16 @@ export interface response {
   styleUrls: ['./managebooks.component.css']
 })
 export class ManagebooksComponent implements OnInit {
-  bookList = [];
   page = 1;
   pageSize=12;
-  mgrid: any;
-   columnDefs = [
-        {headerName: 'Book No', field: 'index'},
-        {headerName: 'Book', field: 'name'},
-        {headerName: 'No. of pages', field: 'numberOfPages'},
-        {headerName: 'No. of characters', field: 'numberOfCharacters'}
-    ];
+  columnDefs = [
+      {headerName: 'Book No', field: 'index'},
+      {headerName: 'Book', field: 'name'},
+      {headerName: 'No. of pages', field: 'numberOfPages'},
+      {headerName: 'No. of characters', field: 'numberOfCharacters'}
+  ];
 
-    rowData = [];
-    // [
-    //     {make: 'Toyota', model: 'Celica', price: 35000},
-    //     {make: 'Ford', model: 'Mondeo', price: 32000},
-    //     {make: 'Porsche', model: 'Boxter', price: 72000}
-    // ];
+  rowData = [];
   constructor(
     private _bookService: BooksService,
     private _router: Router,
@@ -41,17 +34,14 @@ export class ManagebooksComponent implements OnInit {
 
   ngOnInit() {
     this.getAllBookList();
-
-
   }
 
   processData(data) {
     let processedData = [];
     for(let index in data) {
-      let bookId = +data[index].url.split('/').pop()
       processedData.push({
         index: +index + 1,
-        bookId: bookId,
+        bookId: +data[index].url.split('/').pop(),
         name: data[index].name,
         numberOfPages: data[index].numberOfPages,
         numberOfCharacters: data[index].characters.length,
@@ -62,28 +52,16 @@ export class ManagebooksComponent implements OnInit {
 
   getAllBookList() {
     this._bookService.getAllBooks(this.page,this.pageSize).subscribe((data: any) => {
-      this.bookList = data;
       this.rowData = this.processData(data);
     },
       (err: any) => {
         console.log('problem in getting books list');
       })
   }
-
   
   goToBookDetail(event) {
     this._router.navigate(['/bookdetail'], { queryParams: { id: event.data.bookId } })
   }
-
-  getBookNumber(bookInfo) {
-    let bookDetails = bookInfo.split('/');
-    return bookDetails[bookDetails.length - 1];
-  }
-
-  sortData(column) {
-    this.bookList.reverse();
-  }
-
-  
+ 
  
 }
